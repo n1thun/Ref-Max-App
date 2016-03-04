f76ebcf1f982faf5d7521aee5e14e7430c37522a
Nithun <nithun@gmail.com>
2016-03-03 18:10:43 -0600

    init commit 2

--- lwc/settings/base.py
+++ lwc/settings/base.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'lwc.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+print BASE_DIR
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
